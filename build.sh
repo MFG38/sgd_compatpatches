75ebea61bb4019ad66c92c6aee60c995348c88c6
MFG38 <santtu.pesonen@mail.com>
2022-10-29 19:31:11 +0300

    Tiny bit of truncation to build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@
 echo SGD Compatibility Patch Generator
 echo by Santtu "MFG38" Pesonen, 2020
 echo ---------------------------------
-echo Enter name of directory to zip
-read DIR
+read -p " Enter name of directory to zip: " DIR
 
 cd "$DIR"
 7za a -tzip sgd_compat_${DIR}.pk3 *
